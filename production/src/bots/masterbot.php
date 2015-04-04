489158c05cea2dc108fccffcaba519770976a063
ObjectiveTruth <j.mendez@rogers.com>
2015-04-04 09:10:39 -0400

    Masterbot working on testing the friend adder

--- production/src/bots/masterbot.php
+++ production/src/bots/masterbot.php
@@ -5,11 +5,11 @@ require_once __DIR__ . "/../ormbootstrap.php";
 
 abstract class MasterBot{
     const DEBUG = false;
-    private static $isInitialized = false;
-    private static $customerEntity;
-    private static $snapchat_engine;
+    private $isInitialized = false;
+    private $customerEntity;
+    private $snapchat_engine;
 
-    abstract protected function onNewFriend($newFriends);
+    abstract protected function onNewFriendRequest($newFriends);
 
     function __construct(Customer $customerEntity){
         $this->customerEntity = $customerEntity;
@@ -17,25 +17,62 @@ abstract class MasterBot{
 
     function initialize(){
         $botUsername = $this->customerEntity->getBotUsername();
-        $botPassword = $this->customerEntity->getBotPassword();
+        $accountName = $this->customerEntity->getName();
 
-        self::$snapchat_engine = new Snapchat($botUsername, self::DEBUG);
-        $accountDBConnection = new ORMDBConnection($accountName);
-        $accountEntityManager = $accountDBConnection->getEntityManager();
+        $this->snapchat_engine = new Snapchat($botUsername, self::DEBUG);
+        $accountEntityManager = new ORMDBConnection($accountName);
+        $this->isInitialized = true;
     }
 
     function start(){
-        if(!self::$isInitialized){
+        $this->startWithInterval(20);
+    }
+
+    function startForOneCycle(){
+        $this->startWithInterval(30, true);
+        return true;
+    }
+
+    function startWithInterval($Interval, $runOnce = false){
+        if(!$this->isInitialized){
             throw new Exception('Must call .initialize() first');};
+
+        $botPassword = $this->customerEntity->getBotPassword();
+
+        while(true){
+            $this->refreshToken();
+
+            $newFriends = $this->getNewFriends();
+
+            if(count($newFriends) > 0){
+
+                foreach ($newFriends as $friend){
+
+                    $this->onNewFriendRequest($friend);
+                }
+            }
+
+            if($runOnce){break;}
+
+            sleep($Interval);
+        }
+    }
+
+    protected function refreshToken(){
+        $this->snapchat_engine->login($botPassword);
+    }
+
+    protected function getNewFriends(){
+        //Normalize for possible false value, if false return an empty array
+        $thisCouldBeFalse = $this->snapchat_engine->getUncomfirmedFriends();
+        if($thisCouldBeFalse == false){$thisCouldBeFalse = Array();}
+        return $thisCouldBeFalse;
     }
 
     function getCustomerEntity(){
         return $this->customerEntity;
     }
 
-    public function getFriends(){
-        return 'foo';
-    }
 
 }
 ?>
