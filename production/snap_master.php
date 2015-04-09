d16d13f0afb542ddc66841b5e657667f5d90942a
ObjectiveTruth-server <j.mendez@rogers.com>
2015-04-09 19:51:55 -0400

    Adding command line AccountName requirement

--- production/snap_master.php
+++ production/snap_master.php
@@ -2,61 +2,19 @@
 
 require_once __DIR__ . "/src/botfactory.php";
 
-//////////// CONFIG ////////////
-$accountName = "Alex"; //Accountname to use 
-////////////////////////////////
+if($argc < 2){
+    echo "Error: Didn't provide any arguments\n";
+    echo "Usage: php5 snap_master.php <account>\n";
+    echo "<account>: name of account to be used for db and tables\n\n";
+    exit(1);
+}
+
+$accountName = $argv[1];
+
+echo "Using Account: $accountName\n";
 
 $botFactory = new BotFactory();
 $mybot = $botFactory->returnBotForAccount($accountName);
 $mybot->initialize();
-//$mybot->startForOneCycle();
-
-
-//$snapchatBot = new SnapchatBotCustom($accountName);
-
-//Setup variables and DB connections
-//$snapchatBot->initialize();
-
-
-//$friend = new Friend();
-//$friend->setUsername("Caleb");
-//$friend->setPermission(14);
-
-//$entityManager->persist($friend);
-//$entityManager->flush();
-
-
-//$imagePath = "objectivetruth_521045426109293831r.jpg"; // URL or local path to a media file (image or video)
-//$sendTo = array("objectivetruth");
-
-//$snapchat = new Snapchat($username, $debug);
-
-//Login to Snapchat with your username and password
-//$snapchat->login($password);
-
-// Get your friends in an array
-//$friends = $snapchat->getFriends();
-
-//echo "My friends: ";
-//print_r($friends);
-
-// Send snap adding text to your image and 10 seconds
-//$snapchat->send($imagePath, $sendTo, "this is a test :D", 10);
-
-// Set a story
-//$snapchat->setStory($imagePath);
-
-// Set a story adding text to the image and 5 seconds
-//$snapchat->setStory($imagePath, 5, "This is my story");
-
-
-// Get snaps data (Without storing them)
-//$snapchat->getSnaps();
-
-// Automatically downloads Snaps and store it in 2nd argument dir, default is 'anon'
-//$snapchat->getSnaps(true, yhtestor);
-
-// Send chat message to "username"
-//$snapchat->sendMessage("username", "hello from Snap-API!");
-
+$mybot->start();
 ?>
