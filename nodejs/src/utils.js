95e01258cbef364d818636f3ab075db01d1c8b68
ObjectiveTruth-server <j.mendez@rogers.com>
2015-04-11 18:42:26 -0400

    started Abstrating files to utils, more work to come

---
+++ nodejs/src/utils.js
@@ -0,0 +1,6 @@
+exports.printUsageToConsole = function(){
+    console.log("\nUsage: nodejs server.js <AccountName> <PortNumber>");
+    console.log("AccountName: Account primary key for SQL");
+    console.log("PortNumber: Port to listen on");
+};
+
